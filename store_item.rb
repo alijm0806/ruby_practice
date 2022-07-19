b9dbc41b009da62188cf11a58d3c581a1697a5e0
alijm0806 <alijm1104@gmail.com>
2022-07-19 13:39:41 -0500

    create coffe store

---
+++ store_item.rb
@@ -0,0 +1,16 @@
+coffee = {
+  :item => "cappucino",
+  :price => 4.99,
+  :size => "large",
+}
+pastries = {
+  :item => "croissant",
+  :price => 3.99,
+  :size => "standard",
+}
+plates = {
+  :item => "salad",
+  :price => 7.99,
+  :size => "small",
+}
+puts coffee[:item]
