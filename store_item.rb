cd8d03d25741dda348cac43b04ca94183c64e7fd
alijm0806 <alijm1104@gmail.com>
2022-07-19 15:33:38 -0500

    create coffe store

--- store_item.rb
+++ store_item.rb
@@ -15,27 +15,44 @@
 # }
 # puts coffee[:item]
 
+# class Store
+# def initialize(item, price, size)
+# @item = item
+# #     @price = price
+# #     @size = size
+# #   end
+
+# #   def item
+# #     @item
+# #   end
+
+# #   def price
+# #     @price
+# #   end
+
+# #   def size=(add_size)
+# #     @size = add_size
+# #   end
+# # end
+
+# # store1 = Store.new("cappucino", 4.99, "large")
+
+# # puts store1.item
+# # puts store1.price
+# # puts store1.size = ("medium")
+
 class Store
-  def initialize(item, price, size)
-    @item = item
-    @price = price
-    @size = size
-  end
+  attr_reader :item, :price
+  attr_writer :size
 
-  def item
-    @item
-  end
-
-  def price
-    @price
-  end
-
-  def size=(add_size)
-    @size = add_size
+  def initialize(input_options)
+    @item = input_options[:item]
+    @price = input_options[:price]
+    @size = input_options[:size]
   end
 end
 
-store1 = Store.new("cappucino", 4.99, "large")
+store1 = Store.new(item: "cappucino", price: 4.99, size: "large")
 
 puts store1.item
 puts store1.price
